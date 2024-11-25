fd21046e2c847bd49b2f0e6bb628c3c454a71fbf
AmandaAndrade26 <amandacurbani06@gmail.com>
2024-11-24 23:48:16 -0300

    Add pages and Files

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementsByClassName("SetaProximo").addEventListener("click", function(){
+    window.location.href = "Venti.html","Shogun.html"
+})
+document.getElementsByClassName("SetaVoltar").addEventListener("click", function(){
+    window.location.href = "Shogun.html","Venti.html","index.html"
+})
